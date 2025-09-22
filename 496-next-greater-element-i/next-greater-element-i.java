c975958408ad838f1e513bca08977ea714b8834e
Shivang Sharma <shivangs829@gmail.com>
2025-09-22 23:20:52 +0530

    Time: 4 ms (34.96%) | Memory: 43.3 MB (86.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -5,15 +5,22 @@ class Solution {
         int arr[]=new int[n];
         Arrays.fill(arr,-1);
         for(int i=0;i<n;i++){
+            int k=-1;
             for(int j=0;j<m;j++){
                 if(nums1[i]==nums2[j]){
-                    for(int k=j+1;k<m;k++){
-                        if(nums2[k]>nums2[j]){
-                            arr[i]=nums2[k];
-                            break;
-                        }
-                    }
+                     k=nums2[j];
+                    // for(int k=j+1;k<m;k++){
+                    //     if(nums2[k]>nums2[j]){
+                    //         arr[i]=nums2[k];
+                    //         break;
+                    //     }
+                    // }
                 }
+                
+                    if(k!=-1 && nums2[j]>k){
+                        arr[i]=nums2[j];
+                        break;
+                    }
             }
         }
         return arr;
